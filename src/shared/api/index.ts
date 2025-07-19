25082ed9ab53963fbf61928b05d074d9c993c550
katsiev1997 <katsiev1997@gmail.com>
2025-07-20 00:45:24 +0300

    fix base api

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,18 +1,7 @@
 import axios from "axios";
 
-const getBaseURL = () => {
-	if (typeof window !== "undefined") {
-		// Клиентская сторона
-		return "/api";
-	}
-	// Серверная сторона
-	return process.env.NODE_ENV === "production"
-		? process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api"
-		: "http://localhost:3000/api";
-};
-
 export const api = axios.create({
-	baseURL: getBaseURL(),
+	baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
 	headers: {
 		"Content-Type": "application/json",
 	},
