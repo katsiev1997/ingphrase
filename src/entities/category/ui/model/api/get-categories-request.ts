296ddad32ba2da34cbd779dde9998968b8d25697
katsiev1997 <katsiev1997@gmail.com>
2025-07-20 01:04:30 +0300

    fix get categories

--- src/entities/category/ui/model/api/get-categories-request.ts
+++ src/entities/category/ui/model/api/get-categories-request.ts
@@ -1,7 +1,15 @@
 import { api } from "@/shared/api";
 import { Category } from "@prisma/client/wasm";
+import { AxiosError } from "axios";
 
 export const getCategoriesRequest = async (): Promise<Category[]> => {
-	const { data } = await api.get("/categories");
-	return data;
+	try {
+		const { data } = await api.get("/categories");
+		return data;
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			throw new Error(error.response?.data.message);
+		}
+		throw error;
+	}
 };
