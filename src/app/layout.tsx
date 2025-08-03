63dac58c77497491a154012954c917cf60bb8078
katsiev1997 <katsiev1997@gmail.com>
2025-08-04 00:53:48 +0300

    add yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import ReactQueryProvider from "@/shared/providers/react-query-provider";
 import { ThemeProvider } from "@/shared/providers/theme-provider";
 import { NavMenu } from "@/features/nav-menu";
 import { PageTransitionAutoAnimate, ThemeScript } from "@/shared/ui";
+import Script from "next/script";
 
 const montserrat = Montserrat({
 	variable: "--font-geist-sans",
@@ -23,6 +24,8 @@ export const viewport: Viewport = {
 	maximumScale: 1,
 	userScalable: false,
 };
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export default function RootLayout({
 	children,
@@ -43,6 +46,21 @@ export default function RootLayout({
 						<NavMenu />
 					</ReactQueryProvider>
 				</ThemeProvider>
+				{isProduction && (
+					<Script id="metrika-counter" strategy="afterInteractive">
+						{`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(99303003, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });`}
+					</Script>
+				)}
 			</body>
 		</html>
 	);
