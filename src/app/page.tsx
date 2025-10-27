a7dd864ad2b390f52d34ada99d50f3ccc0004155
katsiev1997 <katsiev1997@gmail.com>
2025-10-28 00:03:24 +0300

    fix home page prefetch query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
-import { getCategoriesRequest } from "@/entities/category/ui/model/api/get-categories-request";
-import { CategoriesList } from "@/features/category-list";
+import { getCategoriesRequest } from '@/entities/category/ui/model/api/get-categories-request';
+import { CategoriesList } from '@/features/category-list';
 import {
 	dehydrate,
 	HydrationBoundary,
 	QueryClient,
-} from "@tanstack/react-query";
-import { CategoriesSkeleton } from "@/shared/ui/categories-skeleton";
-import { Suspense } from "react";
-import type { Metadata } from "next";
+} from '@tanstack/react-query';
+import { CategoriesSkeleton } from '@/shared/ui/categories-skeleton';
+import { Suspense } from 'react';
+import type { Metadata } from 'next';
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
-	title: "Категории фраз | IngPhrase",
-	description: "Выберите категорию фраз для изучения ингушского языка",
+	title: 'Категории фраз | IngPhrase',
+	description: 'Выберите категорию фраз для изучения ингушского языка',
 };
 
 export default async function Home() {
 	const queryClient = new QueryClient();
 
-	await queryClient.fetchQuery({
-		queryKey: ["categories"],
+	queryClient.prefetchQuery({
+		queryKey: ['categories'],
 		queryFn: () => getCategoriesRequest(),
 	});
 
